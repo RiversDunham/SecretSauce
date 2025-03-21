30d7f56ea83a775f9ce262fbb074c6dfaadf361d
riversdunham <rivers.dunham6709@stu.mtec.edu>
2025-03-21 12:50:07 -0600

    Forgot to remove some files

--- specs/hamburgerTest.js
+++
@@ -1,9 +0,0 @@
-import Login from '../pageObjects/login';
-import HomePage from '../pageObjects/homePage';
-
-discribe('hamburger menu functionality', () => {
-    it('should click hamburger menu elements', async() => {
-        await Login.enterInfo("standard_user", "secret_sauce");
-        
-    });
-});
